f94df811d872afda0c5af2ded52ddddebc47007a
Will Wesley <will@knze.net>
2022-07-04 09:55:02 -0600

    Tic tac toe board and textures module

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 const Stage = require('stage-js/platform/web');
 
-// Create new app
+const Textures = require('./textures');
+Stage(Textures);
+
 Stage(function(stage) {
 
-  // Set view box
-  stage.viewbox(1000, 1000);
+  stage.viewbox(50,50).pin('handle', -0.5);
+  Stage.image('bg').pin('handle', 0.5).appendTo(stage);
 
-  // Create an image and append it to stage
-  const box = Stage.image('box').appendTo(stage);
-
-  // Align box to center
-  box.pin('align', 0.5);
-
-  // On mouse click...
-  box.on('click', function(point) {
-    // ...tween scale values of this node
-    this.tween().ease('bounce').pin({
-      scaleX : Math.random() + 0.5,
-      scaleY : Math.random() + 0.5
-    });
-  });
-});
-
-// Adding a texture
-Stage({
-  image : 'example.png',
-  textures : {
-    box : { x : 0, y : 129, width : 65, height : 65 }
-  }
 });
---
+++ src/textures.js
@@ -0,0 +1,47 @@
+// Textures
+const Stage = require('stage-js/platform/web');
+const ratio = 20;
+
+module.exports = {
+  textures : {
+    'bg' : Stage.canvas(function(ctx) {
+      this.size(30, 30, ratio);
+      ctx.scale(ratio, ratio);
+      ctx.moveTo(10, 1);
+      ctx.lineTo(10, 29);
+      ctx.moveTo(20, 1);
+      ctx.lineTo(20, 29);
+      ctx.moveTo(1, 10);
+      ctx.lineTo(29, 10);
+      ctx.moveTo(1, 20);
+      ctx.lineTo(29, 20);
+      ctx.lineWidth = 0.3;
+      ctx.lineCap = 'round';
+      ctx.strokeStyle = '#999';
+      ctx.stroke();
+    }),
+    'x' : Stage.canvas(function(ctx) {
+      this.size(10, 10, ratio);
+      ctx.scale(ratio, ratio);
+      ctx.moveTo(2, 2);
+      ctx.lineTo(8, 8);
+      ctx.moveTo(2, 8);
+      ctx.lineTo(8, 2);
+      ctx.lineWidth = 0.5;
+      ctx.lineCap = 'round';
+      ctx.strokeStyle = '#000';
+      ctx.stroke();
+    }),
+    'o' : Stage.canvas(function(ctx) {
+      this.size(10, 10, ratio);
+      ctx.scale(ratio, ratio);
+      ctx.arc(5, 5, 2.4, 0, 2 * Math.PI);
+      ctx.lineWidth = 0.5;
+      ctx.strokeStyle = '#000';
+      ctx.stroke();
+    }),
+    '-' : Stage.canvas(function(ctx) {
+      this.size(10, 10, ratio);
+    })
+  }
+}
