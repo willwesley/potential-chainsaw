eb12dcaac5060e6092a27b03af573d55dc5a3011
Will Wesley <will@knze.net>
2022-07-04 10:18:53 -0600

    Init empty clickable board

--- src/index.js
+++ src/index.js
@@ -8,4 +8,22 @@ Stage(function(stage) {
   stage.viewbox(50,50).pin('handle', -0.5);
   Stage.image('bg').pin('handle', 0.5).appendTo(stage);
 
+  // initial empty board
+  const board = [];
+  for(let x = 0; x < 3; x++) {
+    board[x] = [];
+    for(let y = 0; y < 3; y++) {
+      board[x][y] = makeCell(x, y, function() {
+        board[x][y].image('x');
+      })
+    }
+  }
+
+  function makeCell(x, y, onClick) {
+    return Stage.image('-').appendTo(stage).pin({
+      offsetX: (x - 1) * 10,
+      offsetY: (y - 1) * 10,
+      handle: 0.5
+    }).on('click', onClick);
+  }
 });
