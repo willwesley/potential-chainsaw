4b0c073f4e105a7bae09fddbfb7def05b4aa252c
Will Wesley <will@knze.net>
2022-07-10 10:18:17 -0600

    Fixed bug preventing broadcasting messages, slightly tweaked chatbox some more

--- server/websocketserver.js
+++ server/websocketserver.js
@@ -6,23 +6,29 @@ module.exports = function(server) {
   wss.on('connection', function connection(ws, req) {
     ws.on('message', function message(data) {
       console.log('received: %s', data);
-      wss.clients.forEach(function(client) {
-        if (client.readyState === OPEN) {
-          client.send(...makeMessage(req.socket.remoteAddress, data));
-        }
-      })
+      sendEveryone(makeMessage(req.socket.remoteAddress, data));
     });
 
     ws.send(...makeMessage('SERVER', 'Hello ' + req.socket.remoteAddress));
   });
-}
 
-function makeMessage(who, data) {
-  return [
-    JSON.stringify({
-      data: `${data}`,
-      who: who
-    }),
-    { binary: false }
-  ];
+  function makeMessage(who, data) {
+    return [
+      JSON.stringify({
+        data: `${data}`,
+        who: who
+      }),
+      { binary: false }
+    ];
+  }
+
+  function sendEveryone(message) {
+    wss.clients.forEach(function(client) {
+      if (client.readyState === OPEN) {
+    console.log(message)
+        client.send(...message);
+      }
+    });
+  }
+
 }
