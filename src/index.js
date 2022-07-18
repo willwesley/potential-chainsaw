581659ad4eaec9a96f315c17d79c5a35cb648dd1
guest es <guestes@uccs.edu>
2022-07-18 15:45:09 -0600

    Fiddled with Stage.js

--- src/index.js
+++ src/index.js
@@ -7,20 +7,33 @@ Stage(function(stage) {
   stage.viewbox(1000, 1000);
 
   // Create an image and append it to stage
-  const box = Stage.image('box').appendTo(stage);
-
+  const box = Stage.image("box").appendTo(stage);
+  const square = Stage.image("box").appendTo(stage);
   // Align box to center
   box.pin('align', 0.5);
-
+  square.pin('align', 0.7);
   // On mouse click...
   box.on('click', function(point) {
     // ...tween scale values of this node
     this.tween().ease('bounce').pin({
       scaleX : Math.random() + 0.5,
-      scaleY : Math.random() + 0.5
+      scaleY : Math.random() + 0.5,
+      alignX: Math.random(),
+      alignY: Math.random()
     });
   });
+  square.on('click', function(point) {
+    // ...tween scale values of this node
+    this.tween().ease('bounce').pin({
+      scaleX : Math.random() + 0.5,
+      scaleY : Math.random() + 0.5,
+      alignX: Math.random(),
+      alignY: Math.random()
+    })
+  })
+
 });
+
 
 // Adding a texture
 Stage({
