0ff6dd4eb70636f8200d34a93fea4a429198162c
guest es <guestes@uccs.edu>
2022-07-18 15:44:51 -0600

    Made Many Color Changes.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const Stage = require('stage-js/platform/web');
 Stage(function(stage) {
 
   // Set view box
-  stage.viewbox(1000, 1000);
+  stage.viewbox(500, 500);
 
   // Create an image and append it to stage
   const box = Stage.image('box').appendTo(stage);
@@ -15,17 +15,17 @@ Stage(function(stage) {
   // On mouse click...
   box.on('click', function(point) {
     // ...tween scale values of this node
-    this.tween().ease('bounce').pin({
-      scaleX : Math.random() + 0.5,
-      scaleY : Math.random() + 0.5
+    this.tween(1000).ease('bounce').pin({
+      scaleX : Math.random() + 1.022,
+      scaleY : Math.random() + 1.0675675676621435
     });
   });
 });
-
+  
 // Adding a texture
 Stage({
   image : 'example.png',
   textures : {
-    box : { x : 0, y : 129, width : 65, height : 65 }
+    box : { x : 100, y : .000020, width : 300, height : 100 }
   }
 });
