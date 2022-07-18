c468ca71108af567577180527d5cbc74a4d2f3c1
guest es <guestes@uccs.edu>
2022-07-18 15:42:57 -0600

    Fiddled with stageJS

--- src/index.js
+++ src/index.js
@@ -7,25 +7,57 @@ Stage(function(stage) {
   stage.viewbox(1000, 1000);
 
   // Create an image and append it to stage
-  const box = Stage.image('box').appendTo(stage);
+  const red = Stage.image('red').appendTo(stage);
+  const circle = Stage.image('circle').appendTo(stage);
+  const box = Stage.image('box').appendTo(stage)
 
   // Align box to center
+  red.pin('align', 0.5);
   box.pin('align', 0.5);
+  circle.pin('align', 0.5)
 
   // On mouse click...
-  box.on('click', function(point) {
+  box.on(Stage.Mouse.MOVE, function(point) {
     // ...tween scale values of this node
     this.tween().ease('bounce').pin({
       scaleX : Math.random() + 0.5,
-      scaleY : Math.random() + 0.5
+      scaleY : Math.random() + 0.5,
+      alignX: Math.random() + 0.00001,
+      alignY: Math.random() + 0.00001
+    });
+  });
+  circle.on(Stage.Mouse.MOVE, function(point) {
+    // ...tween scale values of this node
+    this.tween().ease('bounce').pin({
+      scaleX : Math.random() + 0.5,
+      scaleY : Math.random() + 0.5,
+      alignX: Math.random() + 0.00001,
+      alignY: Math.random() + 0.00001
+    });
+  });
+  red.on(Stage.Mouse.MOVE, function(point) {
+    // ...tween scale values of this node
+    this.tween().ease('bounce').pin({
+      scaleX : Math.random() + 0.5,
+      scaleY : Math.random() + 0.5,
+      alignX: Math.random() + 0.00001,
+      alignY: Math.random() + 0.00001
     });
   });
 });
+
 
 // Adding a texture
 Stage({
   image : 'example.png',
   textures : {
-    box : { x : 0, y : 129, width : 65, height : 65 }
+    red : { x : 0, y : 10, width : 80, height : 80},
+    circle : { x : 66, y : 129, width : 100, height : 100},
+    box : { x : 0, y : 129, width : 67, height : 67}
+
   }
 });
+
+
+
+
