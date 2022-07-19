9446bb1098981923281ea1f718338325277ad90a
guest es <guestes@uccs.edu>
2022-07-19 15:49:55 -0600

    Cards

--- src/game.js
+++ src/game.js
@@ -9,26 +9,56 @@ function Game() {
 		}
 	}
 	this.state = {
-		hands: [
-			[' ', ' '],
-		],
-		activePlayer: 'A',
+		hands: [ [], [], [], [], ],
+		activePlayer: 0,
 		turn: 1,
 		outcome: 'In Progress',
 		direction: "L",
 		topcard: this.randomCard()
 	}
-	this.playCard = function(player, card) {
+	this.drawCard = function(player, hands) {
+		this.nextplayer(let(hands = 0 hands < 8))
+
+		this.playCard = function(player, card) {
 		if(this.canPlay(player, card)) {
 			this.state.topcard = card
+			this.nextPlayer()
 		}
+	}
+}
+
+
+	this.drawCard = function(player, hands) {
+		this.state.hands[this.state.activePlayer].push(this.randomCard())
+		this.nextPlayer()
+
 	}
 
 	this.canPlay = function(player, card) {
+		if(this.state.activePlayer !== player) {
+			return false
+		}
 		if(card.color === this.state.topcard.color) {
 			return true
 		}
+		if(card.number === this.state.topcard.number) {
+			return true
+		}
 		return false
+	}
+
+	this.nextPlayer = function() {
+		if(this.state.direction === 'L') {
+			this.state.activePlayer++
+			if(this.state.activePlayer >= this.state.hands.length) {
+				this.state.activePlayer = 0
+			}
+		} else {
+			this.state.activePlayer--
+			if(this.state.activePlayer < 0) {
+				this.state.activePlayer = this.state.hands.length - 1
+			}
+		}
 	}
 }
 Game.COLORS = [
