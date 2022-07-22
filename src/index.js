90bb6a38db5d8c1916943fc23c4f0c0833fdfb0e
guest es <guestes@uccs.edu>
2022-07-22 10:32:44 -0600

    yes

--- src/index.js
+++ src/index.js
@@ -30,15 +30,33 @@ Stage(function(stage) {
   const board = Stage.image("board").appendTo(stage);
   board.pin('align', 0.5)
 
+  const turn = Stage.image('blackturn').appendTo(stage).pin({
+    // scaleToX:100,
+    // scaleToY:100,
+    // scaleMethod:'in',
+    offsetY: 90,
+    offsetX: 90
+  })
 
   function redraw() {
     board.empty()
+    if(game.state.activePlayer === 'R'){
+      turn.image('redturn')
+    } else {
+      turn.image('blackturn')
+    }    
     for(let y in game.state.board) {
       for(let x in game.state.board[y]) {
+        let myX = x, myY = y;
+        if(me === 'R') {
+          myX = 7-x
+          myY = 7-y
+        }
         let piece = Stage.image(game.state.board[x][y]).appendTo(board).scaleTo(100, 100, 'in')
+
         piece.pin({
-          offsetY: 90 + (150*x),
-          offsetX: 90 + (150*y)
+          offsetY: 90 + (150*myX),
+          offsetX: 90 + (150*myY)
         })
         piece.on(Stage.Mouse.START, function(){
           from = [1*x,1*y]
@@ -50,6 +68,11 @@ Stage(function(stage) {
               Math.floor((point.y - 90)/150),
               Math.floor((point.x - 90)/150),
             ]
+            if(me === 'R') {
+              to[0] = 7 - to[0]
+              to[1] = 7 - to[1]
+            }
+            console.log(from, to)
             ws.send(JSON.stringify({place: [from, to]}))
             board.off(Stage.Mouse.MOVE, Move)
             board.off(Stage.Mouse.END, Stop)
@@ -97,5 +120,19 @@ Stage({
   image : 'RedK.png',
   textures : {
     RK : { x : 0, y : 0, width : 512, height : 512 }
+  }
+});
+
+Stage({
+  image : 'redturn.png',
+  textures : {
+    redturn : { x : 0, y : 0, width : 200, height : 200 }
+  }
+});
+
+Stage({
+  image : 'blackturn.png',
+  textures : {
+    blackturn : { x : 0, y : 0, width : 224, height : 224 }
   }
 });
